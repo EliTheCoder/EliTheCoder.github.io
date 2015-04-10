47bb933e3ea35eb0054f309017dc6ece85219214
EX master <EXmaster9@users.noreply.github.com>
2015-04-09 19:48:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ var an4 = document.getElementById('an4');
 function qu1() {
   qu.textContent = q1;
   an1.textContent = a1;
-  an2.textContent = a2;
-  an3.textContent = a3;
-  an4.textContent = a4;
+  an2.textContent = b1;
+  an3.textContent = c1;
+  an4.textContent = d1;
 }
